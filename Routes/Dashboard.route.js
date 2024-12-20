512fd2657393be2b0144747c6c6a6dfc171c6a6b
Gia Bao <baotg0502@gmail.com>
2024-12-20 12:07:28 +0700

    update

--- Routes/Dashboard.route.js
+++ Routes/Dashboard.route.js
@@ -3,6 +3,6 @@ const router = express.Router()
 const DashboardController = require('../Controllers/Dashboard.Controller')
 
 router.get('/stats', DashboardController.getDashboardStats)
-router.get('/dailyChart', DashboardController.getDailyPotholes)
+router.post('/dailyChart', DashboardController.getDailyPotholes)
 
 module.exports = router
